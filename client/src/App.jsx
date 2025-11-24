14f298974889c69e583d4ab788bd9a79ca750fe6
faizan-ahmad5 <fa3n20004@gmail.com>
2025-11-24 20:10:11 +0500

    fix: tailwind setup & wrap App with BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import {Route, Routes} from 'react-router-dom'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
+       <div className='flex h-screen w-screen'>
+    <Sidebar />
+    <Routes>
+      <Route path="/" element={<ChatBox />} />
+      <Route path="/credits" element={<Credits/>} />
+      <Route path="/community" element={<Community />} />
+    </Routes>
+    </div>
+    </div>
+   
+    </>
   )
 }
 
