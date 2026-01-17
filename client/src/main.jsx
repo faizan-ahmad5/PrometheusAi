44e3f74442b7e1e850cb5f061ef2cc3d9d634971
Habibauet <24pwbcs1165@uetpeshawar.edu.pk>
2026-01-18 00:03:30 +0500

    Initialize app with routing, global context, and early theme setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from "react-router-dom";
+import { AppContextProvider } from './context/AppContext.jsx';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+
+    <AppContextProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </AppContextProvider>
+    
+  </BrowserRouter>,
 )
